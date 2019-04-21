25ba7511c2c5876c6129b0ef55b13e51dfed1280
Japa Alekhin Llemos <jap@alekhin.io>
2019-04-21 17:45:01 +0800

    added html5 boilerplate

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#fafafa">
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <p>Hello world! This is HTML5 Boilerplate.</p>
+
+  <?php wp_footer(); ?>
+</body>
+
+</html>
