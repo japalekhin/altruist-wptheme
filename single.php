ce7760bcb143c666d1dc35c2f5163f674f234779
Japa Alekhin Llemos <jap@alekhin.io>
2019-04-22 06:04:27 +0800

    added comments link on article meta hide subtitle for now

--- single.php
+++ single.php
@@ -5,8 +5,10 @@
     <div class="container">
       <div class="article-container">
         <h1 class="article-title"><?php the_title(); ?></h1>
-        <p class="article-subtitle">Drawing graphics on the screen is an essential part of developing games. Sure, some text-based games exist but let’s face it, those are too specialized and only a few people understand how good they are (if they are).</p>
-        <div class="article-meta pt-2 pb-4">article meta</div>
+        <!-- <p class="article-subtitle">Drawing graphics on the screen is an essential part of developing games. Sure, some text-based games exist but let’s face it, those are too specialized and only a few people understand how good they are (if they are).</p> -->
+        <div class="article-meta pt-2 pb-4">
+          <?php if (comments_open()): ?><span class="commentsLink"><?php comments_popup_link(__('Comment', 'devin'), __('1 Comment', 'devin'), __('% Comments', 'devin')); ?></span><?php endif; ?>
+        </div>
       </div>
     </div>
     <div class="article-cover text-center">
