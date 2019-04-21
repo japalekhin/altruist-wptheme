7cea6a1d61dfd9e508d3107fda63160399861624
Japa Alekhin Llemos <jap@alekhin.io>
2019-04-22 02:37:11 +0800

    added cabin and lora from google fonts

--- functions.php
+++ functions.php
@@ -21,7 +21,8 @@ add_action('init', function () {
 
 add_action('wp_enqueue_scripts', function () {
   wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
-  wp_enqueue_style('altruist', get_stylesheet_directory_uri() . '/css/main.css', ['bootstrap',]);
+  wp_enqueue_style('fonts', 'https://fonts.googleapis.com/css?family=Cabin|Lora');
+  wp_enqueue_style('altruist', get_stylesheet_directory_uri() . '/css/main.css', ['bootstrap', 'fonts',]);
 
   wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', [], false, true);
 });
