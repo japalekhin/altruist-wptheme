e7e6a75bfe580cf013d8d533f8f094d78f4b867e
Japa Alekhin Llemos <jap@alekhin.io>
2019-04-21 23:42:01 +0800

    added fixed header and sticky footer

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js h-100" lang="">
 
 <head>
   <meta charset="utf-8">
@@ -9,8 +9,87 @@
   <?php wp_head(); ?>
 </head>
 
-<body>
-  <p>Hello world! This is HTML5 Boilerplate.</p>
+<body class="d-flex flex-column h-100">
+  <header class="fixed-top bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container">
+        <a class="navbar-brand" href="<?php echo home_url(); ?>">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <main class="">
+    <div class="container">
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+      <p>asdf</p>
+    </div>
+  </main>
+
+  <footer class="footer mt-auto py-3 bg-light">
+    <div class="container text-center">
+      footer
+    </div>
+  </footer>
 
   <?php wp_footer(); ?>
 </body>
