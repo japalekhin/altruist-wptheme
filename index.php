cc8ba98205f1820b144b07542cdfc394d64ba980
Japa Alekhin Llemos <jap@alekhin.io>
2019-04-24 17:11:16 +0800

    added home page

--- index.php
+++ index.php
@@ -1,12 +1,31 @@
 <?php get_header(); ?>
-<div class="container">
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis tristique lectus eget consectetur.</p>
-  <p>Quisque semper quam sed euismod rutrum. Phasellus ut tortor urna. Aenean sed iaculis nisl. Fusce vel nulla pharetra, finibus lectus eleifend, egestas arcu. Integer dapibus mollis felis ac imperdiet.</p>
-  <p>Proin vitae dui accumsan eros venenatis ullamcorper.</p>
-  <p>Duis nunc ante, condimentum quis dolor a, malesuada dictum velit. Fusce volutpat sed nisl nec laoreet.</p>
-  <p>Curabitur ac ullamcorper dui, gravida congue nibh. Integer et bibendum tellus. Nulla interdum aliquet imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse condimentum sodales pulvinar.</p>
-  <p>Vestibulum scelerisque nulla ac nisi pretium malesuada. Quisque eu turpis libero. Nam malesuada venenatis justo. Duis dolor quam, ultricies vitae rhoncus at, efficitur eget tellus. Vestibulum dapibus lacus ac justo dignissim, non vulputate lacus elementum. Morbi pharetra justo quis lectus pretium porta eget non arcu. Mauris justo ex, tempus et tempor sit amet, tristique ac nisl. Vivamus ligula elit, dictum a rutrum eget, luctus non turpis. Suspendisse nec mauris eu felis varius cursus in id sem. Donec mauris odio, pellentesque id massa sit amet, malesuada sollicitudin ligula.</p>
-  <p>Donec convallis, odio sed ornare viverra, nisl risus sollicitudin ante, vel dignissim nulla turpis a ligula. Curabitur non iaculis diam. Nam vel lacinia purus, in ornare magna. Duis hendrerit turpis arcu, eget maximus odio rhoncus a. Donec vestibulum mattis convallis. Nullam ex felis, porttitor sit amet lorem vitae, scelerisque lacinia nisi. Ut convallis cursus condimentum. In hac habitasse platea dictumst. Nulla egestas et arcu ut iaculis. Mauris ante velit, laoreet et neque sit amet, sagittis sodales ex. Nam condimentum accumsan tempor.</p>
-  <p>Vestibulum non posuere elit, ut varius lorem. Donec sed quam a eros sodales tempor nec sit amet odio. Maecenas a tincidunt tortor, eu faucibus nisl. Donec hendrerit velit ac pellentesque faucibus.</p>
+<div class="homePosts">
+  <div class="container">
+    <?php while(have_posts()): ?>
+      <?php the_post(); ?>
+      <div class="homePost d-flex flex-column flex-md-row align-items-center align-items-md-start my-5">
+        <a href="<?php the_permalink(); ?>" class="homePostImage mb-2 mb-md-0 mr-md-4 flex-shrink-0"></a>
+        <div class="homePostDetails">
+          <h2 class="homePostTitle">
+            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+          </h2>
+          <p class="homePostSubtitle">Subtitle</p>
+          <div class="homePostMeta">
+            <span class="homePostMetaItem">
+              <i class="far fa-clock mr-1"></i>
+              <?php the_time('l, F j, Y'); ?>
+            </span>
+            <?php if (comments_open()): ?>
+              <i class="far fa-comments mr-1"></i>
+              <span class="commentsLink homePostMetaItem">
+                <?php comments_popup_link(__('Comment', 'devin'), __('1 Comment', 'devin'), __('% Comments', 'devin')); ?>
+              </span>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
+      <hr class="d-lg-none mx-auto mb-4" />
+    <?php endwhile; ?>
+  </div>
 </div>
-<?php get_footer(); ?>
+<?php get_footer();
