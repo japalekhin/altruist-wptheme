7d0106488fd7e6abbfa8c276215fe88a48262c55
Japa Alekhin Llemos <jap@alekhin.io>
2019-05-01 21:56:40 +0800

    post css config added

---
+++ resources/postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = ({ options }) => ({
+  plugins: {
+    'autoprefixer': {},
+    'postcss-preset-env': {},
+    'css-mqpacker': {},
+    'cssnano': options.dev ? false : {
+      preset: [
+        'default',
+        {
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      ]
+    },
+  },
+});
