685177495d20e242de322544e972284b361a982d
Japa Alekhin Llemos <jap@alekhin.io>
2019-05-14 15:30:16 +0800

    cleaned up main.js

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -4,10 +4,4 @@ import Vue from 'vue';
 let vm = new Vue({
   el: '#app',
   template: '#appContent',
-  data: {
-    title: 'yes',
-  },
-  created() {
-    console.log('vue has been created');
-  }
 });
