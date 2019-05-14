c33e2258a16342091555f98c19980ee111d79565
Japa Alekhin Llemos <jap@alekhin.io>
2019-05-14 18:02:27 +0800

    fixed vue import to include compiler+runtime fixed vue initialization wrapped app contents in another div layer added scroll indicator

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,7 +1,28 @@
 import 'bootstrap';
-import Vue from 'vue';
+import Vue from 'vue/dist/vue.esm';
+import debounce from 'debounce';
 
 let vm = new Vue({
-  el: '#app',
+  el: '#appContent',
   template: '#appContent',
+  data: {
+    scrollPercentage: 0,
+  },
+  created() {
+    window.addEventListener('scroll', this.onWindowScroll);
+    this.calculateWindowScroll();
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.onWindowScroll);
+  },
+  methods: {
+    calculateWindowScroll() {
+      let h = document.documentElement, b = document.body, st = 'scrollTop', sh = 'scrollHeight';
+      this.scrollPercentage = ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100) + '%';
+    },
+    onWindowScroll() { this.calculateWindowScroll(); }
+    // onWindowScroll: debounce(function () {
+    //   this.calculateWindowScroll();
+    // }, 30),
+  },
 });
