7e1d6bcde2b1c118feaf6daa86ca25fba0b5cd21
Japa Alekhin Llemos <jap@alekhin.io>
2019-08-24 23:40:30 +0800

    removed dist

--- dist/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// autoloader script
-require 'vendor/autoload.php';
-require 'packages/bootstrap-navwalker.php';
-require 'packages/theme.php';
-require 'packages/helpers.php';
-require 'packages/contact-email.php';
-require 'packages/mailing-list.php';
